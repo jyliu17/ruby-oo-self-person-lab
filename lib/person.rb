23614e4df364a69c062e3f91107b322bc5d80550
jyliu17 <jyliu17@gmail.com>
2020-10-28 12:01:54 -0400

    done

--- lib/person.rb
+++ lib/person.rb
@@ -7,82 +7,98 @@
 # - The happiness and hygiene points should be able to change, however the maximum and minimum points for both happiness and hygiene should be 10 and  0 respectively
 # - The amount in the bank account should also be able to change, though it has no max or min.
 
+
 class Person
 
     attr_reader :name
-    attr_accessor :bank_account, :happiness, :hygiene 
+    attr_accessor :bank_account, :happiness, :hygiene
 
     def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
         @name = name
         @bank_account = bank_account
-        @happiness = happiness
-        @hygiene = hygiene              
+        @happiness = happiness.to_i
+        @hygiene = hygiene.to_i
     end
+
     def happiness=(happiness)
-        if
-        happiness > 10
-        happiness = 10
-        elsif 
-        happiness < 0
-        happiness = 0
-        end
+        
         @happiness = happiness
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness <0
+       
+        # if 
+        # @happiness > 10
+        # @happiness = 10
+        # elsif
+        # @happiness < 0
+        # @happiness = 0
+        # else
+        # @happiness = happiness
+        # end
     end
+
     def hygiene=(hygiene)
-        if
-        hygiene > 10
-        hygiene = 10
-        elsif 
-        hygiene < 0
-        hygiene = 0
-        end
         @hygiene = hygiene
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene <0
     end
 
     def happy?
-        self.happiness > 7 ? true : false
+        @happiness > 7
     end
-
+    
     def clean?
-        self.hygiene > 7 ? true : false
+        @hygiene > 7
     end
-
+        
     def get_paid(salary)
-        self.bank_account += salary
-        "all about the benjamins"
+        @bank_account += salary
+        return "all about the benjamins"
     end
 
     def take_bath
         self.hygiene += 4
-        "♪ Rub-a-dub just relaxing in the tub ♫"
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
+        self.happiness +=2
         self.hygiene -= 3
-        self.happiness += 2
-        "♪ another one bites the dust ♫"
+        return "♪ another one bites the dust ♫"
     end
 
-    def call_friend(name)
+    def call_friend(friend)
         self.happiness += 3
-        name.happiness += 3
-        "Hi #{name.name}! It's #{self.name}. How are you?"
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
-    def start_conversation(name,topic)
-        
-        if topic == "politics"
-            self.happiness -= 2
-            name.happiness -= 2
-            "blah blah partisan blah lobbyist"
-                
-        elsif topic == "weather"
-            self.happiness += 1
-            name.happiness += 1
-            "blah blah sun blah rain"
+    def start_conversation(friend, topic)
 
-        else 
-            "blah blah blah blah blah"
+        if 
+            topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+
+        return "blah blah partisan blah lobbyist"
+
+        elsif
+             topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            
+        return "blah blah sun blah rain"
+
+        else
+
+        return "blah blah blah blah blah"
+        
         end
     end
+
 end
+
+
+# if x <=> min < 0, x = min; 
+#     if x <=> max > 0 , x = max
+#     else x
